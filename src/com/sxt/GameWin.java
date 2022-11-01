989cbbbcb28b9b75971dd01800283d4ca7c8c9c5
Layer <Layer@atguigu.com>
2022-11-01 13:36:58 +0800

    hot-fix 1 commit

--- src/com/sxt/GameWin.java
+++ src/com/sxt/GameWin.java
@@ -151,6 +151,7 @@ public class GameWin extends JFrame {
         System.out.println("修改了");
         System.out.println("hello git");
         System.out.println("hot-fix");
+        System.out.println("hot-fix merge");
 
     }
 }
